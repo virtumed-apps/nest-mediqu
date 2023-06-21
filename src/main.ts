0f124248760e31aa58b64c7cdae72019535816bc
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2023-06-21 20:47:38 -0300

    feat: "added useStact"

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './shared/main/app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  app.useStaticAssets(join(__dirname, '..', 'static'));
   // Habilitando o CORS na aplicação
   app.enableCors({
     credentials: true,
