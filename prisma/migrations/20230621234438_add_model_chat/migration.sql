a7c823055c0582b546c2ab7b4913c3f34d5e58d2
Lázaro Enrico <lazaro.enr.menezes@gmail.com>
2023-06-21 20:46:17 -0300

    feat:"added model chat"

---
+++ prisma/migrations/20230621234438_add_model_chat/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "message" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "message_pkey" PRIMARY KEY ("id")
+);
